418ea0f71f00051382ba1c33a865c39fd2dda4ce
Nape93-max <martinnapetschnig85@gmail.com>
2022-12-05 23:44:44 +0100

    Integration

--- BoltzmannSolver.jl
+++ BoltzmannSolver.jl
@@ -1,5 +1,5 @@
 function fRK4(yn, xn)
-    K1 = -g*xn*yn;
+    K1 = -exp(-xn)/25*(1E10*Yeq*exp(-25*exp(xn))*sinh(yn)+1);
 end
 
 function RK4K2(yn, xn)
@@ -28,18 +28,33 @@ function RK4step(yn, xn)
 end
 
 #Define parameters of RK4 method
-Delta_x = 1E-1;
-x_initial = 0.;
-x_final = 4.;
-Npoints = ceil(Int8,(x_final-x_initial)/Delta_x);
+Delta_x = 1E-6;
+x_initial = -3.;
+x_final = 3;
+Npoints = ceil(Int64,(x_final-x_initial)/Delta_x);
+
+#Define physics parameters of the model
+g_quark = 4;
+m_quark = 1E4;
+Alpha_DM = 0.1; 
+sigma0 = pi*(Alpha_DM/m_quark)^2;
+Yeq = 0.1;
+
+#Define constant physics parameters
+Mpl = 1.221E19;
+H0 = 1.447E-42;
+T0 = 2.35E-13;
+rho_crit = 3.724E-47;
+s0 = 2.225E-38;
+BigConstant = sqrt(pi/45)*Mpl*m_quark;
 
 #First define initial conditions:
-g = 9.81;
-Y0 = 100*ones(Npoints);
-Yx = copy(Y0);
+Y0 = Yeq*ones(Npoints);
+Yx = zeros(Npoints);
 xvec = collect(x_initial:Delta_x:x_final);
 
 for i in 2:Npoints
     Yx[i] = RK4step(Yx[i-1], xvec[i-1]);
 end
 
+Yx
