ed26242dacf30c08b060c7d820b44ada94767997
Nape93-max <martinnapetschnig85@gmail.com>
2022-12-07 17:51:30 +0100

    equilibrum yield updated

--- BoltzmannSolver.jl
+++ BoltzmannSolver.jl
@@ -1,5 +1,6 @@
 using Plots
 using LaTeXStrings
+using SpecialFunctions
 
 function Newton_Raphson_step(t, W_old, cs, g_deg, gS) #Method to calculate the next W=log(Y) point in the implicit backward Euler method.
     # t = log(x) is the t_(n+1) time step, W_old is W_n and cs is Delta_t*lambda(t_(n+1))*geff(t_(n+1)). g_deg is the degeneracy of the annihilating particles.
@@ -23,13 +24,14 @@ function Newton_Raphson_iteration(t, W_old, cs, W_previous, g_deg, gS) #Does one
 end
 
 function Yeq(g_deg, gS, x) #Calculates the equilibrium yield of species with degeneracy g_deg at time x with gS relativistic entropic d.o.f.
-    90/(2*pi)^3.5*g_deg/gS*x*sqrt(x)*exp(-x)
+    #90/(2*pi)^3.5*g_deg/gS*x*sqrt(x)*exp(-x) #This is the non-relativistic version of the line below
+    11.25*g_deg*(x/(pi*pi))^2/gS*besselk(2,x)
 end
 
 #Define parameters of implicit Euler backward solution method
 Delta_t = 1E-5;
 x_initial = 20;
-x_final = 1E2;
+x_final = 1E4;
 t_initial = log(x_initial);
 t_final = log(x_final);
 
@@ -57,7 +59,7 @@ EquilibriumYield = zeros(Npoints);
 for i = 1:Npoints
     EquilibriumYield[i] = Yeq(g_quark, 1, xvec[i])
 end
-Yx = zeros(Npoints);
+
 Wx = zeros(Npoints);
 Yx[1] = EquilibriumYield[1];
 Wx[1] = log(Yx[1]);
@@ -72,8 +74,6 @@ Yx = exp.(Wx)
 plot(xvec, [EquilibriumYield, Yx], title="WIMP freeze-out", label=[L"Y_{eq}(x)" L"Y(x)"], xlabel="x = m/T", ylabel="Y(x)", xaxis=:log, yaxis=:log, xlims = (x_initial, x_final), ylims = (1E-20, 1E-3))
 savefig("FreezeOut.png")
 
-plot(tvec, Wx)
-savefig("W_plot.png")
 
 plot(xvec, Yx, xaxis=:log, yaxis=:log)
 savefig("Y_plot.png")
