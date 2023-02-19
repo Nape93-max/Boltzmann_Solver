bf6395ef1f28d13837f87e4e8541c049bee123bb
Martin Napetschnig <59123424+Nape93-max@users.noreply.github.com>
2023-02-19 23:50:36 +0100

    Add files via upload

--- Exclusion_Plot.jl
+++ Exclusion_Plot.jl
@@ -9,7 +9,7 @@ const BBN_lifetime = 1/1.52*1E-22 #Lower bound on glueball decay rate.
 test_df = DataFrame(CSV.File("V_model_scan.csv")) #Read in data as a dataframe
 
 #Initialise the data in respective arrays
-x_values = test_df[!, 1] #values for m
+x_values = test_df[!, 2] #values for Lambda
 y_values = test_df[!, 3] #values for m/Lambda
 GB_rate_values = test_df[!, 10]
 relic_abundance_values = test_df[!, 12] 
@@ -22,6 +22,6 @@ x_BBN = x_values[correct_BBN_indices]
 y_BBN = y_values[correct_BBN_indices]
 
 #Scatter plot to get a feeling for the result 
-scatter(x_relic, y_relic, title = "V model", minorgrid = true, minorticks = 10, legend=:bottomleft, xscale = :log, yscale = :log, xlabel = L"m_Q/GeV", ylabel = L"\frac{m_Q}{\Lambda}", labels="Overclosure")
+scatter(x_relic, y_relic, title = "V model", minorgrid = true, minorticks = 10, legend=:bottomleft, xscale = :log, yscale = :log, xlabel = L"\Lambda/GeV", ylabel = L"m_Q/\Lambda", labels="Overclosure")
 scatter!(x_BBN, y_BBN, labels="BBN constraint on GB lifetime")
 savefig("Exclusion_plot_Vmodel_scatter_plot.png")
